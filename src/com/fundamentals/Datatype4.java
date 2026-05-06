f780ad499b467c7d3701c7285d717b02805ac1b1
Your Name <you@example.com>
2026-05-06 23:57:50 +0530

    code 4

---
+++ src/com/fundamentals/Datatype4.java
@@ -0,0 +1,29 @@
+package com.fundamentals;
+
+
+//==operates checks content if it is primitive data types.
+//==operates checks object address if it is object data types.
+//if we wnt compare  the content of string object will use.equals()method from string
+public class Datatype4 {
+        
+	public static void main(String[] args) {
+		System.out.println("main method started");
+		 int a=10;
+         int b=10;
+         
+
+		System.out.println(a == b);
+		
+		String str ="Vish";//string literal-->scp:string constant pool.
+		String str1 =new String("Java");//string object:heap memomry.
+		String str2="Vish";
+		String str3 =new String("Guna");//2objects:
+		String str4="Guna";
+
+				
+		System.out.println(str == str1);//false
+		System.out.println();
+		System.out.println(str == str2);//true
+	}
+
+}
